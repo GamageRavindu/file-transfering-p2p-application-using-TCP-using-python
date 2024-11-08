c9ca06fbae95dd6f78bae425ad4d968a19bd5616
Induwara Gamage <induwaragamage2002@gmail.com>
2024-11-08 07:50:45 +0530

    update 2.0

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket 
 import os
+import json
+import base64
 
 if __name__ == "__main__":
     host = 'localhost'
@@ -14,25 +16,34 @@ if __name__ == "__main__":
     if not os.path.exists(save_directory):
         os.makedirs(save_directory)
     
-
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host, port))
     server.listen(totalclients)
 
     print("waiting for clients...")
-
     clientsocket, address = server.accept()
     print(f"Connected with {address} client...")
 
-    filename = f"output.jpg"
-    file_path = os.path.join(save_directory, filename)
+    # initializing variables
+    file_name = None
+    file_path = None
 
-    received_data_chunk = clientsocket.recv(1024) 
-    with open(file_path, "wb") as fo:
-        while received_data_chunk:
-            fo.write(received_data_chunk)
-            received_data_chunk = clientsocket.recv(1024)
-    
-    print("file received from client!")
+    while True:
+        # receive file in chunks
+        with clientsocket.makefile('r') as client_file: #wrap soket in the file for line based reading 
+            for line in client_file:
+                message = json.loads(line.strip()) #parsing each json line
 
-    clientsocket.close()
+                #get file name only once in the first chunk
+                if not file_name and message["file_name"]:
+                    file_name = message["file_name"]
+                    file_path = os.path.join(save_directory, file_name)
+
+                #decode base64 data and write it to the file
+                file_data = base64.b64decode(message["file_data"])
+                with open(file_path, "ab") as fo:  #append binary data to the file
+                    fo.write(file_data)
+                    
+        print(f"file {file_name} received succesfully! from the client: {address}")
+
+        clientsocket.close()
